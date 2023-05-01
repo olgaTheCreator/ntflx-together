167f67eba0a2d216a365ef9fdbd38be8cd3d4de9
ojb <curylo.olga@gmail.com>
2023-05-01 14:53:37 +0100

    deleted if statemenets for dataframes

--- backend/database/load_json_data.py
+++ backend/database/load_json_data.py
@@ -12,13 +12,10 @@ for i in range(0, 12):
             data = json.load(file)
             if data:
                 current_df = pd.json_normalize(data) # type: ignore
-                if current_df:
-                    streamingInfo = pd.json_normalize(current_df["streamingInfo.us.netflix"])[0] # type: ignore
-                    if streamingInfo:
-                        link = pd.json_normalize(streamingInfo)["link"] # type: ignore
-                        if link:
-                            current_df["link"] = link
-                            df = pd.concat([df, current_df]) # type: ignore 
+                streamingInfo = pd.json_normalize(current_df["streamingInfo.us.netflix"])[0] # type: ignore
+                link = pd.json_normalize(streamingInfo)["link"] # type: ignore
+                current_df["link"] = link
+                df = pd.concat([df, current_df]) # type: ignore 
     except IOError:
         print("wasn't able to create dataframe")
     else:
