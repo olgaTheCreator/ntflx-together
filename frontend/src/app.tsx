7d0f5deeb2a68505beee3f769ce5310ce3c83c02
ojb <curylo.olga@gmail.com>
2023-05-15 18:42:17 +0100

    adding cookie works, need register form

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -11,17 +11,20 @@ import { v4 as uuidv4 } from 'uuid';
 
 export function App() {
   const [cookies, setCookie] = useCookies(['ntflx_user']);
-  const [uuid, setUuid] = React.useState();
-  const newCookie = () => {
+  const [uuid, setUuid] = React.useState('');
+  console.log(cookies.ntflx_user);
+  if (!uuid || uuid == undefined) {
     setUuid(uuidv4());
+  }
+  const newCookie = () => {
     setCookie('ntflx_user', uuid, { maxAge: 2592000, sameSite: true });
   };
 
   return (
     <div className="h-screen flex-col justify-center overflow-scroll bg-blue-500 font-poppins text-white">
-      {cookies.ntflx_user ? <span>Hello {cookies.ntflx_user}</span> : <span>No cookie</span>}
+      {cookies.ntflx_user != undefined ? <span>Hello {cookies.ntflx_user}</span> : <span>No cookie</span>}
       <button onClick={newCookie}>press me</button>
-      {console.log(cookies.ntflx_user, uuid)}
+      {/* {console.log(cookies.ntflx_user, uuid)} */}
       {/* <div className="text-lg font-bold text-warning-400"></div> */}
       {/* <div className="flex h-screen max-h-screen max-w-md flex-col items-center justify-between">
         <div className="flex h-7/8 justify-center"> */}
